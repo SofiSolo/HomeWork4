eb8ce45ed4a7f896b334b1418aebac98e177b8c4
SofiSolo <sofa.galaxia@yandex.ru>
2022-08-27 23:07:53 +0300

    задани 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int bananas = 90;
+        System.out.println("Бананы " + bananas + " кг");
+        byte cat = 4;
+        System.out.println("Кошка весит " + cat + " кг");
+        short oil = 3_500;
+        System.out.println("Масло пальмовое " + oil + " литров");
+        long population = 1_650_800;
+        System.out.println("Население города Н " + population + " человек");
+        float coordinate = 0.10f;
+        System.out.println("координата " + coordinate + " градусов");
+        double meaning = 5.2656456;
+        System.out.println("значение " + meaning);
+
     }
 }
